0dcf2e911d4a07e55a99bbc78d9d8b9840126edb
Warrick Lo <wlo@warricklo.net>
2026-06-14 18:18:32 -0700

    Rename top-level module to prevent clashes
    Also fixes linter errors, whitespace, and copyright.
    
    Top-level module name MUST be unique to avoid collisions with other
    projects in the shuttle run.
    
    The port list MUST also exactly follow the Tiny Tapeout template, so a
    comment has been added to emphasise this.
    
    Signed-off-by: Warrick Lo <wlo@warricklo.net>

--- rtl/rv32e_core_wrapper.sv
+++ rtl/rv32e_core_wrapper.sv
@@ -1,8 +1,16 @@
-// Provides a clean, generic bus interface for the core
-// Easier UVM access
+/* SPDX-License-Identifier: CERN-OHL-P-2.0 */
+
+/*
+ * Copyright 2026 UBC ASIC contributors (Montreal project).
+ * All rights reserved.
+ *
+ * Authors: Chathil Rajamanthree <chathil.rajaman3@gmail.com>
+ *
+ * Montreal RV32E Core Wrapper
+ *
+ * Provides a clean, generic bus interface for the core for easier UVM access.
+ */
 
 module rv32e_core_wrapper ();
 
-
-
-endmodule
+endmodule : rv32e_core_wrapper
