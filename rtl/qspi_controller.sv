e88cee43f355fbc5df7b910689143bcb6077e2f8
Chat <63841542+chatrajaman3@users.noreply.github.com>
2026-06-14 17:04:43 -0700

    map pins

--- rtl/qspi_controller.sv
+++ rtl/qspi_controller.sv
@@ -1,4 +1,5 @@
 // Interface between core and QSPI pmod 
+// https://onlinedocs.microchip.com/oxy/GUID-450989FA-38E4-4D68-AB61-15ADB29AD718-en-US-6/GUID-C2190631-B6F5-4CD7-B6DB-5267DC280E90_3.html
 
 module qspi_controller (
     // Clock
@@ -12,6 +13,25 @@ module qspi_controller (
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
 );
+
+    // ====================================
+    //              Pin mapping
+    // ====================================
+
+    // QSPI Serial CLK
+
+    // QSPI CS - Active Low
+
+    // QSPI IO_0
+    // QSPI IO_1
+    // QSPI IO_2
+    // QSPI IO_3
+
+
+    logic       qspi_clk;
+    logic       qspi_cs_n;
+    logic [3:0] qspi_data;
+
 
 
 
