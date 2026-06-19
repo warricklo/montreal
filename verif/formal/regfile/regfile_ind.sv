78bd0c7835c49373cbb2731ccda9346ebf397d2a
Warrick Lo <wlo@warricklo.net>
2026-06-18 23:20:11 -0700

    Add regfile formal verification tests
    Successfully verifies the following requirements:
    * DR-REGFILE-020
    * DR-REGFILE-041
    * DR-REGFILE-050
    * DR-REGFILE-051
    
    In progress:
    * DR-REGFILE-043
    
    Signed-off-by: Warrick Lo <wlo@warricklo.net>

--- verif/formal/regfile/regfile_ind.sv
+++ verif/formal/regfile/regfile_ind.sv
@@ -42,12 +42,22 @@ module regfile_ind (
   );
 
   /* Assert reset (active low) and clear register.
-   * Requirement DR-REGFILE-053 cannot be verified in this module
-   * as we need to verify the behaviour of register 0 without
-   * any prior reset assertions. */
+   * Requirement DR-REGFILE-053 cannot be verified in this module as we need
+   * to verify the behaviour of register 0 without any prior reset assertions. */
 
   initial assume (rst_ni == '0);
   initial assume (register == '0);
+
+  /* Some assertions use $past(), which requires at least one clock edge.
+   * Those tests will use past_valid to check if a valid history exists. */
+
+  logic past_valid;
+
+  initial assume (past_valid == '0);
+
+  always_ff @(posedge clk_i) begin
+    past_valid <= '1;
+  end
 
   /* DR-REGFILE-010:
    * The module must store NUM_WORDS words, each XLEN bits wide.
@@ -58,6 +68,32 @@ module regfile_ind (
 
   initial assert ($bits(register) == 16 * 32);
 
+  /* DR-REGFILE-020:
+   * Slice k of word n shall correspond to bits [(k+1)*SLICE_WIDTH−1:k*SLICE_WIDTH]
+   * of register n, partitioning each word into XLEN/SLICE_WIDTH
+   * non-overlapping slices, each SLICE_WIDTH bits wide.
+   *
+   * DR-REGFILE-050:
+   * The read output rdata_o[i] shall be combinationally derived from
+   * the current register state, raddr_i[i], and slice_sel_i. There shall not
+   * be any latency on reads.
+   *
+   * DR-REGFILE-051:
+   * The read output rdata_o[i] must reflect the slice selected by slice_sel_i
+   * within the register selected by raddr_i[i].
+   *
+   * Notes: This verification module assumes XLEN = 32 and SLICE_WIDTH = 8.
+   *        These three requirements are easiest to verify together. */
+
+  always_comb begin
+    if (raddr_i[0] != '0) begin
+      assert (rdata[0] == register[raddr_i[0]][(slice_sel_i + 1) * 8 - 1 : slice_sel_i * 8]);
+    end
+    if (raddr_i[1] != '0) begin
+      assert (rdata[1] == register[raddr_i[1]][(slice_sel_i + 1) * 8 - 1 : slice_sel_i * 8]);
+    end
+  end
+
   /* DR-REGFILE-030:
    * Register contents must be 0 at most one clock after the
    * synchronous reset is asserted. */
@@ -65,6 +101,15 @@ module regfile_ind (
   always_ff @(posedge clk_i) begin
     if ($past(!rst_ni, 1)) begin
       assert (register == '0);
+    end
+  end
+
+  /* DR-REGFILE-041:
+   * Writes to the register file must only occur when wen_i is asserted. */
+
+  always_ff @(posedge clk_i) begin
+    if (past_valid && $past(rst_ni && !wen_i, 1)) begin
+      assert ($stable(register));
     end
   end
 
